7f100eb61a74b69df678a6d3105b704ed6c1e4da
nguyetha79 <nguyet-ha1.phung@stu.uni-regensburg.de>
2024-05-01 20:07:40 +0200

    update

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -11,24 +11,23 @@ const FriendListWidget = ({ userId }) => {
   const token = useSelector((state) => state.token);
   const friends = useSelector((state) => state.user.friends);
 
-  const getFriends = async () => {
-    const response = await fetch(
-      `https://social-media-webapp2.onrender.com/users/${userId}/friends`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    const data = await response.json();
-    dispatch(setFriends({ friends: data }));
-  };
-
   useEffect(() => {
+    const getFriends = async () => {
+      const response = await fetch(
+        `https://social-media-webapp2.onrender.com/users/${userId}/friends`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const data = await response.json();
+      dispatch(setFriends({ friends: data }));
+    };
     getFriends();
-  }, []);  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // eslint-disable-next-line react-hooks/exhaustive-deps
 
   return (
     <WidgetWrapper>
